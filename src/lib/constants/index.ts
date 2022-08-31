92269b3e71c65ccd8c912e63f14b739ec3c6e49e
kari <ammr.arf@gmail.com>
2022-08-31 12:35:49 +0800

    🎨 change dump dir

--- src/lib/constants/index.ts
+++ src/lib/constants/index.ts
@@ -1,4 +1,5 @@
 import { TiMaluumPageLinkCollections } from "../types";
+import os from 'node:os'
 
 export const IMALUUM_LOGIN_PAGE =
 	"https://cas.iium.edu.my:8448/cas/login?service=https%3a%2f%2fimaluum.iium.edu.my%2fhome";
@@ -11,7 +12,9 @@ export const IMALUUM_SUBPAGE_LINKS: TiMaluumPageLinkCollections = {
 	TEST: "",
 };
 
-export const DUMP_BASE_PATH = "./tmp";
+const tmpDir = os.tmpdir();
+
+export const DUMP_BASE_PATH = `${tmpDir}/imaluum-cli`;
 
 export const CREDENTIALS_FILE_PATH = `${DUMP_BASE_PATH}/credentials.json`;
 
